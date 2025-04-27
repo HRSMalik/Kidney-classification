287de577511a7e4afbb81fe64fb99d8ee3a7493b
Harris <hrsmalik1798@gmail.com>
2025-04-28 03:38:52 +0500

    fix: move STAGE_NAME assignment inside the main block for proper scoping

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from CNNClassifier import logger
+from CNNClassifier.pipeline.stage02_prep_base_model import PrepareBaseModelPipeline
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
 
-STAGE_NAME = "Data Ingestion stage"
 
 if __name__ == "__main__":
+        STAGE_NAME = "Data Ingestion stage"
+
         try:
             logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
             obj = DataIngestionPipeline()
+            obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+        except Exception as e:
+            logger.exception(e)
+            raise e
+        
+        STAGE_NAME = "Prepare Base Model stage"
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = PrepareBaseModelPipeline()
             obj.main()
             logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
         except Exception as e:
