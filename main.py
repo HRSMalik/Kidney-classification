71ccae87243ede6873e716ce9434ee83ad042328
Harris <hrsmalik1798@gmail.com>
2025-05-01 16:30:41 +0500

    fix: correct import order and ensure proper execution flow for model training stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CNNClassifier import logger
 from CNNClassifier.pipeline.stage02_prep_base_model import PrepareBaseModelPipeline
-from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from CNNClassifier.pipeline.stage03_modeltrain import ModelTrainingPipeline
+from CNNClassifier.pipeline.stage01_data_ingestion import DataIngestionPipeline
 
 
 
@@ -20,6 +21,16 @@ if __name__ == "__main__":
         try:
             logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
             obj = PrepareBaseModelPipeline()
+            obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+        except Exception as e:
+            logger.exception(e)
+            raise e
+        
+        STAGE_NAME = "Model Training stage"
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = ModelTrainingPipeline()
             obj.main()
             logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
         except Exception as e:
