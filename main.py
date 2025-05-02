425746104c6d55707e4a96a00c346da6e7c252cc
Harris <hrsmalik1798@gmail.com>
2025-05-02 20:17:54 +0500

    commnted out the model training pipeline

--- main.py
+++ main.py
@@ -4,35 +4,34 @@ from CNNClassifier.pipeline.stage03_modeltrain import ModelTrainingPipeline
 from CNNClassifier.pipeline.stage01_data_ingestion import DataIngestionPipeline
 
 
-
 if __name__ == "__main__":
-        STAGE_NAME = "Data Ingestion stage"
+    STAGE_NAME = "Data Ingestion stage"
 
-        try:
-            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-            obj = DataIngestionPipeline()
-            obj.main()
-            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-        except Exception as e:
-            logger.exception(e)
-            raise e
-        
-        STAGE_NAME = "Prepare Base Model stage"
-        try:
-            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-            obj = PrepareBaseModelPipeline()
-            obj.main()
-            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-        except Exception as e:
-            logger.exception(e)
-            raise e
-        
-        STAGE_NAME = "Model Training stage"
-        try:
-            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-            obj = ModelTrainingPipeline()
-            obj.main()
-            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-        except Exception as e:
-            logger.exception(e)
-            raise e
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    STAGE_NAME = "Prepare Base Model stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    STAGE_NAME = "Model Training stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        # obj = ModelTrainingPipeline()
+        # obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
